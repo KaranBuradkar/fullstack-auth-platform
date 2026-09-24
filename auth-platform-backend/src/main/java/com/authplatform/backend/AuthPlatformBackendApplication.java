344e0b862ab2eabcfe5b40785de4124e758fa77a
karanburadkar <karanburadka30@gmail.com>
2026-09-25 00:48:06 +0530

    chore: initial project setup

---
+++ auth-platform-backend/src/main/java/com/authplatform/backend/AuthPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.authplatform.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ auth-platform-backend/src/test/java/com/authplatform/backend/AuthPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authplatform.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
